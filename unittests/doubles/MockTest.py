080978f028752b5a5b3958a4d9b3be003a6b1b19
teiniker <egon.teiniker@gmail.com>
2020-03-04 21:59:36 +0100

    Initial commit.

---
+++ unittests/doubles/MockTest.py
@@ -0,0 +1,50 @@
+import unittest
+from unittest.mock import Mock
+
+class MockTest(unittest.TestCase):
+
+    def setUp(self):
+        self.json = Mock()  # Mock() replaces any object
+
+    def testCalled(self):
+        # exercise
+        self.json.loads('{"key": "value"}')
+
+        # verify
+        self.json.loads.assert_called()
+        self.json.loads.assert_called_once()
+        self.json.loads.assert_called_with('{"key": "value"}')
+        self.json.loads.assert_called_once_with('{"key": "value"}')
+
+
+    def testCalledOnce(self):
+        # exercise
+        self.json.loads('{"key": "value"}')
+        self.json.loads('{"key": "value"}')
+
+        # verify
+        with self.assertRaises(AssertionError): # loads() is called twice
+            self.json.loads.assert_called_once()
+
+
+    def testCalledCount(self):
+        # exercise
+        self.json.loads('{"key": "value"}')
+        self.json.loads('{"key": "value"}')
+
+        # verify
+        self.assertEqual(2, self.json.loads.call_count)
+
+
+# When writing tests, it is important to ensure that the results are predictable.
+# You can use Mock to eliminate uncertainty from your code during testing.
+
+    def testReturnValue(self):
+        # setup
+        self.json.random.return_value = 666
+
+        # exercise
+        randomValue = self.json.random()
+
+        # verify
+        self.assertEqual(666, randomValue)
---
+++ unittests/doubles/README_MOCK.md
@@ -0,0 +1,12 @@
+# Mock Framework
+
+When you substitute an object in your code, the **Mock** must look like the real 
+object it is replacing. Otherwise, your code will not be able to use the *Mock* 
+in place of the original object.
+
+
+## References
+[unittest.mock](https://docs.python.org/3/library/unittest.mock-examples.html)
+
+*Egon Teiniker, 2020, GPL v3.0*
+
