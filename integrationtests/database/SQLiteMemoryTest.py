d8ec01152852aad3a252d106c404e94eb3d465ea
teiniker <egon.teiniker@gmail.com>
2020-03-25 20:47:40 +0100

    Initial commit.

---
+++ integrationtests/database/SQLiteMemoryTest.py
@@ -0,0 +1,83 @@
+import unittest
+import sqlite3
+
+class SQLiteMemoryTest(unittest.TestCase):
+    # We can use the special name :memory: to create a database in RAM.
+    # The database ceases to exist as soon as the database connection is closed.
+    # Every :memory: database is distinct from every other. So, opening two
+    # database connections each with the filename ":memory:" will create two
+    # independent in-memory databases.
+
+    DATABALE_NAME = ':memory:'
+
+    def setUp(self):
+        self.conn = sqlite3.connect(SQLiteMemoryTest.DATABALE_NAME)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE user (id INTEGER, username TEXT, password TEXT, PRIMARY KEY(id))")
+        self.cur.execute("INSERT INTO user (id,username, password) VALUES (1, 'homer', '2aaab795b3836904f82efc6ca2285d927aed75206214e1da383418eb90c9052f')")
+        self.cur.execute("INSERT INTO user (id,username, password) VALUES (2, 'marge', 'b4b811fa40505329ae871e52f03527c3720c9af7fb8607819658535c5484c41e')")
+        self.cur.execute("INSERT INTO user (id,username, password) VALUES (3, 'bart', '9551dadbf76a27457946e70d1aebebe2132f8d3bce6378d216c11853524dd3a6')")
+        self.cur.execute("INSERT INTO user (id,username, password) VALUES (4, 'lisa', 'd84fe7e07bedb227cffff10009151d96fc944f6a1bd37cff60e8e4626a1eb1c3')")
+        self.cur.execute("INSERT INTO user (id,username, password) VALUES (5, 'maggie', 'aae5be5f6474904b686f639e0fcfd2be440121cd889fa381a94b71750758345e')")
+
+    def tearDown(self):
+        self.conn.close();
+
+    def testSelectAllUsers(self):
+        self.cur.execute('SELECT * from user')
+        table = self.cur.fetchall()
+        # Verify
+        self.assertEqual(5, len(table))
+        for row in table:
+            print(row)
+
+    def testSelectAllUsersOrderedByUsername(self):
+        self.cur.execute('SELECT * from user ORDER BY username')
+        table = self.cur.fetchall()
+        # Verify
+        self.assertEqual(5, len(table))
+        self.assertEqual('marge', table[0][1])
+        self.assertEqual('homer', table[1][1])
+        self.assertEqual('lisa', table[2][1])
+        self.assertEqual('maggie', table[3][1])
+        self.assertEqual('marge', table[4][1])
+
+    def testSelectAllUsersUsernameLike(self):
+        parameters = ('m%',)
+        # Exercise
+        self.cur.execute("SELECT * from user WHERE username LIKE ?", parameters)
+        table = self.cur.fetchall()
+        # Verify
+        self.assertEqual(2, len(table))
+        self.assertEqual('marge', table[0][1])
+        self.assertEqual('maggie', table[1][1])
+
+
+    def testSelectUser(self):
+        # Setup
+        parameters = ('bart',)
+        # Exercise
+        self.cur.execute("SELECT * from user where username=?", parameters)
+        table = self.cur.fetchall()
+        # Verify
+        self.assertEqual(1, len(table))
+        record = table[0]
+        self.assertEqual(3, record[0])        # id
+        self.assertEqual('bart', record[1])   # username
+        self.assertEqual('9551dadbf76a27457946e70d1aebebe2132f8d3bce6378d216c11853524dd3a6', record[2])  # password
+
+    def testAuthentication(self):
+        # Setup
+        parameters = ('lisa', 'd84fe7e07bedb227cffff10009151d96fc944f6a1bd37cff60e8e4626a1eb1c3')
+        # Exercise
+        self.cur.execute("SELECT * from user where username=? AND password=?", parameters)
+        table = self.cur.fetchall()
+        # Verify
+        self.assertEqual(1, len(table)) # Authenticated
+
+if __name__ == '__main__':
+    unittest.main()
+
+# References:
+# https://www.sqlite.org/inmemorydb.html
+# https://www.sqlitetutorial.net/sqlite-like/
---
+++ integrationtests/filesystem/JsonFileTest.py
@@ -0,0 +1,42 @@
+import unittest
+import os
+import json
+
+class JsonFileTest(unittest.TestCase):
+    FILE_PATH = 'data_file.json'
+
+    def setUp(self):
+        data = {'person': {'name': 'John', 'age': 30, 'city': "New York"}}
+        with open(self.FILE_PATH, "w") as write_file:
+            json.dump(data, write_file, indent=2)
+
+    def tearDown(self):
+        os.remove(self.FILE_PATH)
+
+    def testDeserializationFromFile(self):
+        # Exercise
+        with open(self.FILE_PATH, "r") as read_file:
+            data = json.load(read_file)
+        # Verify
+        expected = {'person': {'name': 'John', 'age': 30, 'city': 'New York'}}
+        self.assertDictEqual(expected, data)
+
+    def testModifyJsonFile(self):
+        # Setup
+        data = {'person': {'name': 'John', 'age': 30, 'city': "New York"}}
+        # Exercise
+        data['person']['age'] = 50
+        with open(self.FILE_PATH, "w") as write_file:
+            json.dump(data, write_file, indent=2)
+        # Verify
+        with open(self.FILE_PATH, "r") as read_file:
+            data = json.load(read_file)
+        expected = {'person': {'name': 'John', 'age': 50, 'city': 'New York'}}
+        self.assertDictEqual(expected, data)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# References:
+# https://docs.python.org/3/library/json.html#module-json
+# https://realpython.com/python-json/
