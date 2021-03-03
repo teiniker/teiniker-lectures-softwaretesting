45c9c663b2e928f4b883d2a4e1cf5af3b1519c29
teiniker <egon.teiniker@gmail.com>
2021-03-03 16:56:09 +0100

    Refactoring.

--- debugging/insertion_sort/insertion_sort.py
+++ debugging/insertion_sort/insertion_sort.py
@@ -1,4 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s : %(message)s')
+log = logging.getLogger(__name__)
+
 def insertion_sort(arr):  
+    log.debug(arr)    
     # Traverse through 1 to len(arr) 
     for i in range(1, len(arr)): 
         key = arr[i] 
@@ -6,12 +12,14 @@ def insertion_sort(arr):
         # greater than key, to one position ahead 
         # of their current position 
         j = i-1
+        log.debug('  key={} i={} j={}'.format(key,i,j))
         while j >=0 and key < arr[j] : 
                 arr[j+1] = arr[j] 
+                log.debug('    j={}: arr[{}] = arr[{}] = {}'.format(j,j+1,j,arr[j]))                
                 j -= 1
         # Bug: arr[j] = key 
         arr[j+1] = key 
-
+        log.debug('  {}'.format(arr))        
 # Verify 
 list_of_nums = [5, 2, 1, 8, 4]
 insertion_sort(list_of_nums)
