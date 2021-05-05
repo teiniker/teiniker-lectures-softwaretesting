862d4fc5ff6fb3a5baf674019bd2ae0c6a8703bb
teiniker <egon.teiniker@gmail.com>
2021-05-05 13:52:50 +0200

    Refactoring.

--- testing/unittests/test_case_design/input_validation_exercise/input_validation_test.py
+++ testing/unittests/test_case_design/input_validation_exercise/input_validation_test.py
@@ -3,8 +3,3 @@ import unittest
 from user import User, ValidationError
 
 # TODO: Implement test cases for a code coverage of 100%
-
-# $ coverage3 run -m unittest input_validation_test_exercise.py
-# $ coverage3 report 
-# $ coverage3 html
-
