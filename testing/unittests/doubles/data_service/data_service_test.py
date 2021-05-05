34a6d2c42f2b60c1bb2ce190d5ce1e785723aabd
teiniker <egon.teiniker@gmail.com>
2021-05-05 13:16:01 +0200

    Refactoring.

--- testing/unittests/doubles/data_service/data_service_test.py
+++ testing/unittests/doubles/data_service/data_service_test.py
@@ -29,23 +29,6 @@ class DataServiceTest(unittest.TestCase):
             # Exercise
             self.service.csvData()
 
-
-    def testCsvData2(self):
-        # Setup
-        self.dao = Mock()  # Mock() replaces DataAccessObject
-        self.dao.readData.return_value = [0.8273, 0.7822, 0.9731, 0.1239, 0.9898]
-        self.service = DataService(self.dao)
-
-        # Exercise
-        values = self.service.csvData()
-
-        # State verification
-        expected = "0.8273,0.7822,0.9731,0.1239,0.9898"
-        self.assertEqual(expected, values)
-        # Behavioral verification
-        self.dao.readData.assert_called_once()
-        self.assertEqual(1, self.dao.readData.call_count)    
-
 if __name__ == '__main__':
     unittest.main()
 
