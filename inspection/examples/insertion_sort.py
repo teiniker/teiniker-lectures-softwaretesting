84c373dd7c3264341fe72fdc8c4b44738bb8e7a8
teiniker <egon.teiniker@gmail.com>
2022-03-04 14:01:57 +0100

    Initial import.

---
+++ inspection/examples/insertion_sort.py
@@ -0,0 +1,13 @@
+def Insertion_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while (i >=0) and (A[i] > key):
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = key
+
+
+x = [2,7,3,8,1] 
+Insertion_sort(x)
+print(x)
