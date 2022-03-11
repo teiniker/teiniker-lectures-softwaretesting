1272b6cc2deb92baac4b63b2c30a31ef880d6deb
teiniker <egon.teiniker@gmail.com>
2022-03-11 08:32:10 +0100

    Initial import.

--- inspection/examples/functions/bubble_sort.py
+++ inspection/examples/functions/bubble_sort.py
@@ -1,17 +1,23 @@
-def bubbleSort(nums):
-    direction = False
+"""
+PyLint Example: How to fix PyLint warnings.
+"""
+
+def bubble_sort(nums):
+    """
+    Implementation of the bubble sort algorithm.
+    """
     swapped = True
 
     while swapped:
-      swapped = False
-      for i in range(len(nums) - 1):
+        swapped = False
+        for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
-                swapped = True;
+                swapped = True
 
 
 # Verify
 random_list_of_nums = [5, 2, 1, 8, 4]
-bubbleSort(random_list_of_nums)
+bubble_sort(random_list_of_nums)
 print(random_list_of_nums)
 assert random_list_of_nums == [1, 2, 4, 5, 8]
