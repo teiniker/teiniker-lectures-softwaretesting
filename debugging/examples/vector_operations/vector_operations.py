3a563130b7862c811c96ad163c3391e7d6bf4239
teiniker <egon.teiniker@gmail.com>
2022-04-01 11:31:49 +0200

    Initial import.

---
+++ debugging/examples/vector_operations/vector_operations.py
@@ -0,0 +1,62 @@
+from pyrsistent import v
+
+def vector_print(vector, msg):
+    print(f'{msg}: {vector}')
+
+def vector_add(v1, v2):
+    result = []
+    # Bug: if len(v1) == len(v2):
+    if len(v1) != len(v2):
+        return result
+    for i in range(len(v1)):
+        # Bug: result.append(v1[i] + v1[i])
+        result.append(v1[i] + v2[i])
+    return result
+
+def vector_sub(v1, v2):
+    result = []
+    if len(v1) != len(v2):
+        return result
+    for i in range(len(v1)):
+        # Bug: result.append(v2[i] - v1[i])
+        result.append(v1[i] - v2[i])
+    return result
+
+def vector_inverse(vector):
+    result = list()
+    size = len(vector)
+    for i in range(size):
+        # Bug: result.append(vector[size-i])
+        result.append(vector[size-1-i])
+    return result
+
+def vector_scalar_product(v1, v2):
+    result = 0 
+    for i in range(len(v1)):
+        # Bug: result = v1[i] * v2[i]
+        result += v1[i] * v2[i]
+    return result
+
+
+# Verification
+
+v1 = [1,2,3,4,5,6,7,8,9]
+v2 = [10,20,30,40,50,60,70,80,90]
+
+vector_print(v1, 'V1')
+
+v_add = vector_add(v1, v2)
+vector_print(v_add, 'V1+V2')
+assert [1+10, 2+20, 3+30, 4+40, 5+50, 6+60, 7+70, 8+80, 9+90] == v_add
+
+v_sub = vector_sub(v2, v1)
+vector_print(v_sub, 'V2-V1')
+assert [10-1, 20-2, 30-3, 40-4, 50-5, 60-6, 70-7, 80-8, 90-9] == v_sub
+
+v_inv = vector_inverse(v1)
+vector_print(v_inv, 'inv(V1)')
+assert [9, 8, 7, 6, 5, 4, 3, 2, 1] == v_inv
+
+v_scalar = vector_scalar_product(v1, v2)
+vector_print(v_scalar, 'V1 * V2')
+assert 1*10 + 2*20 + 3*30 + 4*40 + 5*50 + 6*60 + 7*70 + 8*80 + 9*90 == v_scalar
---
+++ debugging/examples/vector_operations/vector_operations_exercise.py
@@ -0,0 +1,53 @@
+from pyrsistent import v
+
+def vector_print(vector, msg):
+    print(f'{msg}: {vector}')
+
+def vector_add(v1, v2):
+    result = []
+    if len(v1) == len(v2):
+        return result
+    for i in range(len(v1)):
+        result.append(v1[i] + v1[i])
+    return result
+
+def vector_sub(v1, v2):
+    result = []
+    if len(v1) != len(v2):
+        return result
+    for i in range(len(v1)):
+        result.append(v2[i] - v1[i])
+    return result
+
+def vector_inverse(vector):
+    result = list()
+    size = len(vector)
+    for i in range(size):
+        result.append(vector[size-i])
+    return result
+
+def vector_scalar_product(v1, v2):
+    result = 0 
+    for i in range(len(v1)):
+        result = v1[i] * v2[i]
+    return result
+
+
+# Verification
+
+v1 = [1,2,3,4,5,6,7,8,9]
+v2 = [10,20,30,40,50,60,70,80,90]
+
+vector_print(v1, 'V1')
+
+v_add = vector_add(v1, v2)
+vector_print(v_add, 'V1+V2')
+
+v_sub = vector_sub(v2, v1)
+vector_print(v_sub, 'V2-V1')
+
+v_rev = vector_inverse(v1)
+vector_print(v_rev, 'inv(V1)')
+
+v_scalar = vector_scalar_product(v1, v2)
+vector_print(v_scalar, 'V1 * V2')
