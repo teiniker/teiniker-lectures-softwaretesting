9425584d7c93949eb639af9eccc1db3f3a9430e8
teiniker <egon.teiniker@gmail.com>
2022-04-06 21:21:29 +0200

    Refactoring.

--- testing/unittests/basics/comparator_test/comparator_test_exercise.py
+++ testing/unittests/basics/comparator_test/comparator_test_exercise.py
@@ -1,10 +1,5 @@
 import unittest
 
-# Exercise: Function Test - Comparator
-#
-# Implement all test cases to completely test the compare()
-# function (test all paths through the given function)
-
 def compare(a, b):
     if a > b:
         return 1
--- testing/unittests/basics/resistor_test/resistor_test_exercise.py
+++ testing/unittests/basics/resistor_test/resistor_test_exercise.py
@@ -2,14 +2,6 @@ import unittest
 
 from resistor import Resistor
 
-# Exercise: Class Test 
-# 
-# Implement the following test cases:
-#   > Verify the class variable vendor
-#   > Create an instance Resistor(470, 5) and verify value and tolerance
-#   > Create an instance Resistor(1000) and verify value and tolerance
-#   > Add two Resistor instances Resistor(1000) + Resistor(330) and verify 
-#     the total resistance.
 
 class ResistorTest(unittest.TestCase):
     pass
