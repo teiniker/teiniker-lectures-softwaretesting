1f473b5ac31e08eec270d34e9e730b94bb8ca1b2
teiniker <egon.teiniker@gmail.com>
2022-05-18 21:48:10 +0200

    Refactoring.

--- testing/integrationtests/test-case-design/integer_value/integer_value.py
+++ testing/integrationtests/test-case-design/integer_value/integer_value.py
@@ -1,13 +1,10 @@
-import re
-
 
 class ValidationError(Exception):
     pass
 
 
 def operation(value):
-    print('operation({})'.format(value))
+    print(f"operation({value})")
     if value < 1 or value > 99:
         raise ValidationError('Invalid data value!')
     return value
-
--- testing/integrationtests/test-case-design/integer_value/integer_value_test.py
+++ testing/integrationtests/test-case-design/integer_value/integer_value_test.py
@@ -8,15 +8,15 @@ from integer_value import operation, ValidationError
 
 class IntegerValueTest(unittest.TestCase):
 
-    def testValidNumbers(self):       
+    def test_valid_numbers(self):
         self.assertEqual(1, operation(1))
         self.assertEqual(99, operation(99))
 
-    def testInValid_TooSmall(self):
+    def test_invalid_too_small(self):
         with self.assertRaises(ValidationError):
             operation(0)
 
-    def testInValid_TooLarge(self):
+    def test_invalid_too_large(self):
         with self.assertRaises(ValidationError):
             operation(100)
 
