d0d5fa8d5628bfbd5d3fd05c13afd9be4f12c61e
Egon Teiniker <egon.teiniker@gmail.com>
2023-04-25 20:18:06 +0200

    Refactoring.

--- introduction/python/libraries/files/binary_file/binfile_read_write.py
+++ introduction/python/libraries/files/binary_file/binfile_read_write.py
@@ -2,5 +2,5 @@ with open('data.bin','rb') as in_file:
     data = in_file.read()
     print(data)
 
-    with open('tmp.bin','wb') as out_file:
-        out_file.write(data)
+with open('tmp.bin','wb') as out_file:
+    out_file.write(data)
